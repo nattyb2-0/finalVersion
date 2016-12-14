6e0ea0828ac60cfd8ad6ed2a2ed59c8a30461dcf
Natty Davis <natty.davis79@gmail.com>
2016-12-14 09:02:35 -0500

    all working

--- src/components/Forms/NewLike.jsx
+++ src/components/Forms/NewLike.jsx
@@ -18,9 +18,10 @@ console.log(like)
   render(){
     return(
         <div>
-          <img id={this.props.btnid} onClick={this.createLike} className='likebtn' src='https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcRW5q-djUMEITi99CoY7bLOLf06Dc9OFEKAi0PPBjSIksRhTVT4AQ' />
+          <img  onClick={this.createLike} className='likebtn' src='https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcRW5q-djUMEITi99CoY7bLOLf06Dc9OFEKAi0PPBjSIksRhTVT4AQ' />
         </div>
       )
   }
 }
 
+// id={this.props.btnid}
