57a199ed668cf1cea1f016e3818133e3027ec683
Natty Davis <natty.davis79@gmail.com>
2016-12-14 21:05:20 -0500

    feeling frisky right now...bout to burn the house down

--- models/db.js
+++ models/db.js
@@ -1,14 +1,28 @@
 
-const pg       = require('pg-promise')();
+// const pg       = require('pg-promise')();
 
-const pgConfig =  process.env.DATABASE_URL ||
+// const pgConfig =  process.env.DATABASE_URL ||
 
-               {  host:     process.env.PG_HOST,
-                   port:     process.env.PG_PORT,
-                   database: process.env.PG_DATABASE,
-                   user:     process.env.PG_USER
-                  };
+//                {  host:     process.env.PG_HOST,
+//                    port:     process.env.PG_PORT,
+//                    database: process.env.PG_DATABASE,
+//                    user:     process.env.PG_USER
+//                   };
 
-const db       = pg(pgConfig);
+// const db       = pg(pgConfig);
+
+// module.exports = db;
+
+const pgp = require('pg-promise')();
+
+const config = {
+  host:       'localhost',
+  port:       5432,
+  database:   'project4',
+  user:       'nattydavis',
+
+};
+const db = pgp(config);
 
 module.exports = db;
+
